a0e24d1f95d4b5afa05bc2fe31ca6ca575c1371c
cupid <290870573@qq.com>
2017-02-07 19:35:11 +0800

    Initial commit

---
+++ app/src/main/java/application/MyApplication.java
@@ -0,0 +1,25 @@
+package application;
+
+import android.app.Application;
+
+import com.taobao.weex.InitConfig;
+import com.taobao.weex.WXEnvironment;
+import com.taobao.weex.WXSDKEngine;
+
+import adapter.ImageAdapter;
+
+/**
+ * Created by wondercupid on 2017/1/27.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        WXEnvironment.addCustomOptions("appName","TBSample");
+        WXSDKEngine.initialize(this,new InitConfig.Builder()
+                .setImgAdapter(new ImageAdapter())
+                .build());
+    }
+
+}
