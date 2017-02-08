2ebb00e21209cb29a7e3317b99a6c40317dca888
cupid <290870573@qq.com>
2017-02-08 13:29:58 +0800

    Weex测试demo

--- app/src/main/java/application/MyApplication.java
+++ app/src/main/java/application/MyApplication.java
@@ -15,6 +15,7 @@ public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
+        int abc = 1;
         super.onCreate();
         WXEnvironment.addCustomOptions("appName","TBSample");
         WXSDKEngine.initialize(this,new InitConfig.Builder()
